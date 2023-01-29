83d7f6ac7b04a909a3a6ce685fee94b2cfe1b197
Xiao Han <xiao2@michaels.com>
2023-01-29 11:09:08 +0800

    remove Cargo.lock

--- test/testvec/src/main.rs
+++ test/testvec/src/main.rs
@@ -1,13 +1,16 @@
-
 fn main() {
   let mut list: Vec<i32> = vec![1, 2, 3];
   list.push(4);
-  for i in &list {
-    println!("{}", i);
-  }
 
   println!("--------------------------> {}", list[1]);
-  let  border: i32 = 44;
+
+  for i in list {
+    if i % 2 == 0 {
+      println!("{} is odd", i);
+    }
+  }
+  
+  let border: i32 = 44;
   if border > 15 {
     println!("{} > 15", border);
   }
